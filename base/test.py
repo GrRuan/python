043f0d4f889917b68284ee8057e37f8a97e8acfa
“guoaolei” <429137155@qq.com>
2023-03-22 13:11:34 +0800

    commit

---
+++ base/test.py
@@ -0,0 +1,15 @@
+print("Hello World")
+num1=11
+num2=11.345
+print("数字%5d,%7.2f"%(num1,num2))
+print("数字%d,%f"%(num1,num2))
+
+name1="tom"
+name2="tom"
+print(f"ss{name1==name2}")
+print(name1==name2)
+
+num1=10
+if num1>=10:
+    print("是")
+print("否")
